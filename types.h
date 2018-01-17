c86bbdc976eaffe49859ee1512225676d98adf4b
Rafael Hidalgo Alejo <32574570+rafa9811@users.noreply.github.com>
2018-01-17 22:51:15 +0100

    Add files via upload

---
+++ types.h
@@ -0,0 +1,17 @@
+/* 
+ Madrid, 21 de febrero de 2017.
+ Autores: Carlos Luis Hojas García-Plaza y Rafael Hidalgo Alejo. Pareja 02.
+ PROGRAMACIÓN II - ESCUELA POLITÉCNICA SUPERIOR DE LA UNIVERSIDAD AUTÓNOMA DE MADRID
+ PRÁCTICA 1 
+ */
+
+#ifndef TYPES_H
+#define TYPES_H
+typedef enum {
+ERROR = 0, OK = 1
+} Status;
+typedef enum {
+FALSE = 0, TRUE = 1
+} Bool;
+#endif
+/* TYPES_H */
